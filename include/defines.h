e09e653c2d0b0ea5618e9a7f7424257e9efd49a4
EnderNight <ndxendernight@gmail.com>
2023-03-30 17:57:22 +0200

    Added defines header

---
+++ include/defines.h
@@ -0,0 +1,7 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define SECTION_TEXT_SIZE 20
+#define TEXT_SIZE 200
+
+#endif // !DEFINES_H
--- include/parse.h
+++ include/parse.h
@@ -5,13 +5,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define SECTION_TEXT_SIZE 20
-#define TEXT_SIZE 200
+#include "item.h"
 
-typedef struct item {
-  char *text;
-} item;
-
-void open_item(item *it);
-void read_string(int fd, item *it);
+void open_item(Item *it);
+void read_string(int fd, Item *it);
 #endif
