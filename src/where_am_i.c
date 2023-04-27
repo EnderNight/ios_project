5c6b75ed2473d6db97ae6beac384d3ba74472f20
Aziz Zeghal <101135665+Tofeha@users.noreply.github.com>
2023-04-27 13:09:50 +0000

    whereami sys calls

--- src/where_am_i.c
+++ src/where_am_i.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <utils.h>
+
 void where_am_i(void) {
     char cwd[1024];
     getcwd(cwd, sizeof(cwd));
-    printf("You are in the street: %s\n", cwd);
+    print("You are in the street: %s\n", cwd);
 }
 
 int main(void) { where_am_i(); }
