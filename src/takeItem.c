406568f83798ea9d2dca7ffa388d5ce209598eb1
sdlt2003 <sdlt2003@gmail.com>
2023-05-08 12:39:11 +0200

    inventory directory updated takeItem done (without testing)

--- src/takeItem.c
+++ src/takeItem.c
@@ -1,38 +1,64 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <stdbool.h>
 
-void move_item_file(const char* source_dir, const char* dest_dir) {
-    char cwd[1024];
-    cwd = getcwd(cwd, sizeof(cwd));
+int user_rank = 0;
+
+void takeItem() {
+    const char* initial_path = "./";                // Path (cwd)
+    const char* dest_path = "story/inventory/";     // Path to inventory
+
     DIR* dir;
     struct dirent* entry;
     char filename[20];
     char source_path[100];
-    char dest_path[100];
+    char dest_item_path[100];
 
-    // Open the source directory
-    dir = opendir(source_dir);
+    // open initial path
+    dir = opendir(initial_path);
     if (dir == NULL) {
-        fprintf(stderr, "Error opening directory\n");
+        fprintf(stderr, "Error opening the initial path\n");
         return;
     }
 
-    // Iterate over the directory entries
+    bool found_item = false;
+
+    // iterate over the files in the directory
     while ((entry = readdir(dir)) != NULL) {
         strcpy(filename, entry->d_name);
-        snprintf(source_path, sizeof(source_path), "%s/%s", source_dir, filename);
 
-        if (strcmp(filename, "flamethrower") == 0 ||
-            strcmp(filename, "knife") == 0 ||
-            strcmp(filename, "car_battery") == 0) {
-            snprintf(dest_path, sizeof(dest_path), "%s/%s", dest_dir, filename);
-            rename(source_path, dest_path);
+        // check if the file is an item
+        if (strcmp(filename, "knife.item") == 0 ||
+            strcmp(filename, "flamethrower.item") == 0 ||
+            strcmp(filename, "car_battery.item") == 0) {
+            snprintf(source_path, sizeof(source_path), "%s%s", initial_path, filename);
+            snprintf(dest_item_path, sizeof(dest_item_path), "%s%s", dest_path, filename);
+
+            // move the item to the inventory
+            if (rename(source_path, dest_item_path) == 0) {
+                found_item = true;
+                printf("You took: %s\n", filename);
+                // Incrementa el user_rank según el item encontrado
+                if (strcmp(filename, "knife.item") == 0 && user_rank == 0) {
+                    user_rank = 1;
+                } else if (strcmp(filename, "flamethrower.item") == 0 && user_rank == 1) {
+                    user_rank = 2;
+                } else if (strcmp(filename, "car_battery.item") == 0 && user_rank == 2) {
+                    user_rank = 3;
+                }
+            } else {
+                fprintf(stderr, "Error moving item: %s\n", filename);
+            }
         }
     }
 
     closedir(dir);
+
+    // if no items were found
+    if (!found_item) {
+        printf("There is nothing here.\n");
+    }
 }
